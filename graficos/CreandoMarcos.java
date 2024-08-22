014cab1e3aea670d45dab7734c856126090bd890
Alberto Miranda Pacheco <albert59.gs@gmail.com>
2024-08-22 11:27:05 -0600

    Explicando mas metodos importantes para el uso de Frames en java

--- graficos/CreandoMarcos.java
+++ graficos/CreandoMarcos.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class CreandoMarcos {
     public static void main(String[] args) {
@@ -7,12 +8,20 @@ public class CreandoMarcos {
         marco1.setVisible(true);
         //Metodo que define el comportamiento al cerrar la ventana
         marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //marco1.setBounds(500,500,200,300);
     }
 }
 
 class MiMarco extends JFrame {
     public MiMarco (){
         //Establecer el tamaño de la ventana
-        setSize(500,300);
+        //setSize(500,300);
+
+        //setLocation(500,300); Metodo que establece el lugar donde aparecera el frame
+        setBounds(600,300,550,250); //Este metodo nos permite establecer el lugar y el tamaño del frame
+        //setResizable(false); //Metodo que no permite la modificacion de tamaño del frame
+        //setExtendedState(Frame.MAXIMIZED_BOTH); metodo estatico que Maximiza el frame
+
+        setTitle("Mi nuevo Frame");//Este metodo nos permite poner un titulo en la ventana
     }
 }
