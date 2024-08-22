d04880dea2061bdab02af05230070c679141df19
Alberto Miranda Pacheco <albert59.gs@gmail.com>
2024-08-22 10:30:06 -0600

    Creando nuestro primer frame

--- graficos/CreandoMarcos.java
+++ graficos/CreandoMarcos.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class CreandoMarcos {
     public static void main(String[] args) {
+        MiMarco marco1 = new MiMarco();
+        //Metodo para hacer visible la ventana
+        marco1.setVisible(true);
+        //Metodo que define el comportamiento al cerrar la ventana
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
 
+class MiMarco extends JFrame {
+    public MiMarco (){
+        //Establecer el tamaño de la ventana
+        setSize(500,300);
     }
 }
